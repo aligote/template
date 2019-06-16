c2cd837f54c3d3aaf4a0f5d787598c40921c281a
aligote <merlo555@yandex.ru>
2019-06-16 20:09:40 +0300

    init git

---
+++ js/js.js
@@ -0,0 +1,14 @@
+// (function(){
+//   "use strict";
+
+// var btn = document.querySelector(".top-humburger");
+// console.log(btn);
+
+
+
+// });
+
+window.onload = function() {
+    var btn = document.querySelector(".top-humburger");
+console.log(btn);
+  };
